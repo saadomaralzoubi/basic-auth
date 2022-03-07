a21e9425d100436c29371a449833ce5432b85d37
saadalzoubi <saad.alzoubi.1994@gmail.com>
2022-03-08 01:35:40 +0200

    add

--- src/models/index.js
+++ src/models/index.js
@@ -6,19 +6,19 @@ require("dotenv").config();
 const POSTGRES_URL =
   process.env.NODE_ENV === "test" ? "sqlite:memory:" : process.env.DATABASE_URL;
 
-// let sequelizeOptions =
-//   process.env.NODE_ENV === "production"
-//     ? {
-//         dialectOptions: {
-//           ssl: {
-//             require: true,
-//             rejectUnauthorized: false,
-//           },
-//         },
-//       }
-//     : {};
+let sequelizeOptions =
+  process.env.NODE_ENV === "production"
+    ? {
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        },
+      }
+    : {};
 
-let sequelize = new Sequelize(POSTGRES_URL, {});
+let sequelize = new Sequelize(POSTGRES_URL, sequelizeOptions);
 
 module.exports = {
   db: sequelize,
